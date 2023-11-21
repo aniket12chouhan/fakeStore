43ee9dab4f537ce51651eff34514ae5b4bc12945
aniket12chouhan <aniket12chouhan@gmail.com>
2023-11-21 22:09:15 +0530

    first commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    
+    <footer>
+        <h4>All Rights Reserved &copy; MY.STORE</h4>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
